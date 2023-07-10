801c963f5171a2b22c420f29a218ee810edcc1c0
Lee <leevanrooyen17@gmail.com>
2023-07-10 17:15:48 +0200

    Flutter course til 71 done

--- learningApp/quiz_app/quiz_app_advanced_basics/lib/questions_screen.dart
+++ learningApp/quiz_app/quiz_app_advanced_basics/lib/questions_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app_advanced_basics/answer_button.dart';
+import 'package:quiz_app_advanced_basics/data/questions.dart';
+
+import 'package:quiz_app_advanced_basics/models/questions_screen.dart';
+
 class Questions extends StatefulWidget {
   const Questions({super.key});
 
@@ -10,8 +15,32 @@ class Questions extends StatefulWidget {
 }
 
 class _QuestionsState extends State<Questions> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(context) {
-    return const Text('Questions Screen ');
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answer, () {});
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
