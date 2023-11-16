fe493eceb784b93f9995c50bc80511b6a6a5cf28
Lee <leevanrooyen17@gmail.com>
2023-11-16 14:38:30 +0200

    Update to 108

--- learningApp/expense_tracker/lib/widgets/expenses.dart
+++ learningApp/expense_tracker/lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses%20list/expenses_list.dart';
 
@@ -25,12 +26,18 @@ class _ExpensesState extends State<Expenses> {
         catagory: Catagory.leisure),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add)),
         ],
         title: const Text('Flutter Expense Tracker'),
       ),
